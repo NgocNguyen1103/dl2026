fe58c2eb196553050b80a399187dbf5a87115008
NgocNguyen1103 <quangquangngoc@gmail.com>
2026-05-09 14:22:23 +0700

    Lab3 - Code (incomplete)

---
+++ Labwork3.py
@@ -0,0 +1,57 @@
+data = []
+
+with open('loan2.csv', 'r') as file:
+    for line in file:
+        experience, salary, loan = line.split(',')
+        data.append([float(experience), float(salary), float(loan)])
+
+
+for _ in range(len(data)):
+    print(data[_])
+
+
+def ln(x):
+    n = 1000.0
+    return n * ((x ** (1/n)) - 1)
+
+def exp(x):
+    e = 2.718281828459045
+    return e**x
+
+
+def w0_df(x1, x2, y, w0, w1, w2):
+    inner_sigmoid = 0 - (w1*x1 + w2*x2 + w0)
+    if (inner_sigmoid) < 0.5:
+        inner_sigmoid = 0
+    else: inner_sigmoid = 1
+    loss = 1 - y - inner_sigmoid
+    return loss
+
+def w1_df(x1, x2, y, w0, w1, w2):
+    inner_sigmoid = 0 - (w1*x1 + w2*x2 + w0)
+    if (inner_sigmoid) < 0.5:
+        inner_sigmoid = 0
+    else: inner_sigmoid = 1
+    loss = (0-y)*x1 + x1*(1 - inner_sigmoid)
+    return loss
+
+def w2_df(x1, x2, y, w0, w1, w2):
+    inner_sigmoid = 0 - (w1*x1 + w2*x2 + w0)
+    if (inner_sigmoid) < 0.5:
+        inner_sigmoid = 0
+    else: inner_sigmoid = 1
+    loss = (0-y)*x2 + x2*(1 - inner_sigmoid)
+    return loss
+
+def single_loss_value(x1, x2, y, w0, w1, w2):
+    return
+
+def grad(learning_rate, w0, w1, w2, total_loss):
+    while True:
+        for _ in range(len(data)):
+            x1 = data[_][0]
+            x2 = data[_][1]
+            y = data[_][2]
+            w0 = w0 - learning_rate*w0_df(x1, x2, y, w0, w1, w2)
+            w1 = w1 - learning_rate*w1_df(x1, x2, y, w0, w1, w2)
+            w2 = w2 - learning_rate*w2_df(x1, x2, y, w0, w1, w2)
