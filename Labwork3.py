3e358e989ae3e383af68d6f8cc7f6dae1ec96897
NgocNguyen1103 <quangquangngoc@gmail.com>
2026-05-09 16:07:38 +0700

    Lab3 - Plotting - incomplete

--- Labwork3.py
+++ Labwork3.py
@@ -5,8 +5,9 @@ with open('loan2.csv', 'r') as file:
         experience, salary, loan = line.split(',')
         data.append([float(experience), float(salary), float(loan)])
 
-
+y_list = []
 for _ in range(len(data)):
+    y_list.append(data[_][2])
     print(data[_])
 
 def ln(x):
@@ -58,10 +59,11 @@ def grad(learning_rate, w0, w1, w2, total_loss):
         total_loss = loss/len(data)
         print(f'w0: {w0:.3f} | w1: {w1:.3f} | w2: {w2:.3f}')
         print(f'loss: {total_loss}')
-        if total_loss < 0.3:
+        if total_loss < 0.22:
             return w0, w1, w2
-            
-
+predict_0 = []     
+predict_1 = []
+predict = []
 w0, w1, w2 = grad(0.0001, 0, 1, 2, 0)
 for _ in range(len(data)):
     x1 = data[_][0]
@@ -69,8 +71,25 @@ for _ in range(len(data)):
     y = sigmoid(w1*x1 + w2*x2 + w0)
     if y < 0.5:
         y = 0
-    else: y = 1
+        predict_0.append([x1,x2])
+    else: 
+        y = 1
+        predict_1.append([x1,x2])
+    predict.append(y)
     print(x1, x2, y)
+ 
+counter = 0
+for _ in range(len(predict)):
+    if predict[_] == y_list[_]:
+        counter += 1
+accuracy = counter/len(predict)
+print(f'Accuracy: {accuracy} | {counter}/{len(predict)}')
 
+import matplotlib.pyplot as plt
+x1, y1 = zip(*predict_0)
+x2, y2 = zip(*predict_1)
+plt.scatter(x1,y1, color = 'r')
+plt.scatter(x2,y2, color = 'b')
+plt.show()
 
 
