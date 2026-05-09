ef86c3aa49e6f87909adec935c8be401460340b5
NgocNguyen1103 <quangquangngoc@gmail.com>
2026-05-09 14:44:43 +0700

    Lab3 - Code - Missing Plot

--- Labwork3.py
+++ Labwork3.py
@@ -18,36 +18,36 @@ def exp(x):
     e = 2.718281828459045
     return e**x
 
+def sigmoid(x):
+    if x < 0.5:
+        return 0
+    else: return 1
 
 def w0_df(x1, x2, y, w0, w1, w2):
     inner_sigmoid = 0 - (w1*x1 + w2*x2 + w0)
-    if (inner_sigmoid) < 0.5:
-        inner_sigmoid = 0
-    else: inner_sigmoid = 1
+    inner_sigmoid = sigmoid(inner_sigmoid)
     loss = 1 - y - inner_sigmoid
     return loss
 
 def w1_df(x1, x2, y, w0, w1, w2):
     inner_sigmoid = 0 - (w1*x1 + w2*x2 + w0)
-    if (inner_sigmoid) < 0.5:
-        inner_sigmoid = 0
-    else: inner_sigmoid = 1
+    inner_sigmoid = sigmoid(inner_sigmoid)
     loss = (0-y)*x1 + x1*(1 - inner_sigmoid)
     return loss
 
 def w2_df(x1, x2, y, w0, w1, w2):
     inner_sigmoid = 0 - (w1*x1 + w2*x2 + w0)
-    if (inner_sigmoid) < 0.5:
-        inner_sigmoid = 0
-    else: inner_sigmoid = 1
+    inner_sigmoid = sigmoid(inner_sigmoid)
     loss = (0-y)*x2 + x2*(1 - inner_sigmoid)
     return loss
 
 def single_loss_value(x1, x2, y, w0, w1, w2):
-    return
+    loss = (0 - y)*(w1 * x1 + w2 * x2 + w0) + ln(1 + exp(w1*x1 + w2*x2 +w0))
+    return loss
 
 def grad(learning_rate, w0, w1, w2, total_loss):
     while True:
+        loss = 0
         for _ in range(len(data)):
             x1 = data[_][0]
             x2 = data[_][1]
@@ -55,3 +55,19 @@ def grad(learning_rate, w0, w1, w2, total_loss):
             w0 = w0 - learning_rate*w0_df(x1, x2, y, w0, w1, w2)
             w1 = w1 - learning_rate*w1_df(x1, x2, y, w0, w1, w2)
             w2 = w2 - learning_rate*w2_df(x1, x2, y, w0, w1, w2)
+            single_lost = single_loss_value(x1, x2, y, w0, w1, w2)
+            loss = loss + single_lost
+        total_loss = loss/len(data)
+        print(f'w0: {w0:.3f} | w1: {w1:.3f} | w2: {w2:.3f}')
+        print(f'loss: {total_loss}')
+        if total_loss < 0.6:
+            return w0, w1, w2
+            
+
+w0, w1, w2 = grad(0.0001, 0, 1, 2, 0)
+for _ in range(len(data)):
+    x1 = data[_][0]
+    x2 = data[_][1]
+    y = sigmoid(w1*x1 + w2*x2 + w0)
+    print(x1, x2, y)
+
